d7d1206244387482ab29a8113405a931df100155
mpvermette <marie-pier.vermette@hotmail.com>
2023-07-10 19:55:50 -0400

    dfdsfds

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
 //Update cache names any time any of the cached files change.
-const CACHE_NAME = 'static-cache-v1';
+const CACHE_NAME = 'static-cache-v3';
 
 //Add list of files to cache here.
 const FILES_TO_CACHE = [
@@ -9,6 +9,12 @@ const FILES_TO_CACHE = [
 self.addEventListener('install', (evt) => {
 console.log('[ServiceWorker] Install');
 // Precache static resources here.
+evt.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+    console.log('[ServiceWorker] Pre-caching offline page');
+    return cache.addAll(FILES_TO_CACHE);
+    })
+);
 self.skipWaiting();
 });
 
